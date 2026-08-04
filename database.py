390c31bef8ff8e6e3302373253ed531e0e745df3
slafka1996-creator <slafka1996@gmail.com>
2026-08-05 01:33:43 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,40 +2,50 @@ import sqlite3
 
 
 def connect():
+    return sqlite3.connect("cellcity.db")
 
-    return sqlite3.connect(
-        "cellcity.db"
+
+def init_db():
+
+    db = connect()
+    cur = db.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS players(
+
+        id INTEGER PRIMARY KEY,
+
+        money INTEGER,
+
+        people INTEGER,
+
+        houses INTEGER
+
     )
+    """)
+
+    db.commit()
+    db.close()
+
 
 
 def create_player(user_id):
 
     db = connect()
-
     cur = db.cursor()
 
 
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS players(
-        id INTEGER PRIMARY KEY,
-        money INTEGER,
-        people INTEGER,
-        houses INTEGER
-    )
-    """)
-
-
     cur.execute(
-    """
-    INSERT OR IGNORE INTO players
-    VALUES(?,?,?,?)
-    """,
-    (
-        user_id,
-        1000,
-        0,
-        0
-    )
+        """
+        INSERT OR IGNORE INTO players
+        VALUES(?,?,?,?)
+        """,
+        (
+            user_id,
+            1000,
+            0,
+            0
+        )
     )
 
 
@@ -46,17 +56,59 @@ def create_player(user_id):
 
 def get_player(user_id):
 
-    db=connect()
+    db = connect()
+    cur = db.cursor()
 
-    cur=db.cursor()
 
     cur.execute(
-        "SELECT * FROM players WHERE id=?",
+        """
+        SELECT *
+        FROM players
+        WHERE id=?
+        """,
         (user_id,)
     )
 
-    result=cur.fetchone()
+
+    player = cur.fetchone()
+
 
     db.close()
 
-    return result
+    return player
+
+
+
+def update_player(
+    user_id,
+    money,
+    people,
+    houses
+):
+
+    db = connect()
+    cur = db.cursor()
+
+
+    cur.execute(
+        """
+        UPDATE players
+
+        SET money=?,
+            people=?,
+            houses=?
+
+        WHERE id=?
+
+        """,
+        (
+            money,
+            people,
+            houses,
+            user_id
+        )
+    )
+
+
+    db.commit()
+    db.close()
