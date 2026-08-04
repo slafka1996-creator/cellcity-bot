cc443b8a1eb460d71b156f2a3d7f9ef80b39db44
slafka1996-creator <slafka1996@gmail.com>
2026-08-05 01:05:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,97 @@
+from flask import Flask,request
+import vk_api
+
+from database import *
+
+
+app=Flask(__name__)
+
+
+TOKEN="ТОКЕН_ГРУППЫ"
+
+
+vk=vk_api.VkApi(
+    token=TOKEN
+).get_api()
+
+
+
+@app.route("/",methods=["POST"])
+def callback():
+
+
+    data=request.json
+
+
+    if data["type"]=="confirmation":
+
+        return "СТРОКА_ПОДТВЕРЖДЕНИЯ"
+
+
+
+    if data["type"]=="message_new":
+
+
+        user_id=data["object"]["message"]["from_id"]
+
+        text=data["object"]["message"]["text"].lower()
+
+
+
+        if text=="старт":
+
+
+            create_player(user_id)
+
+
+            answer="""
+🏙 Добро пожаловать в CellCity!
+
+Ваш город создан.
+
+💰 Деньги: 1000
+👥 Жители: 0
+
+Команды:
+
+🏠 дом
+📊 город
+"""
+
+
+        elif text=="дом":
+
+
+            answer="""
+🏠 Вы построили дом!
+
++10 жителей
+-100 монет
+"""
+
+
+        else:
+
+            answer="Напишите: старт"
+
+
+
+        vk.messages.send(
+
+            user_id=user_id,
+
+            message=answer,
+
+            random_id=0
+
+        )
+
+
+    return "ok"
+
+
+
+app.run(
+host="0.0.0.0",
+port=10000
+)
